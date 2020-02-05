5d47428dbe463f70bd02127d535b545872d5e644
Jamie0 <jamie+git@index.hm>
2020-02-05 17:28:35 +0000

    build

---
+++ precache-manifest.71d3e0d2b887ed0af91e24f060196dd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cd2f6e25a3d9549cfbd047a210e9e0",
+    "url": "/MultiCall/index.html"
+  },
+  {
+    "revision": "e87bf140b417d74f3f2e",
+    "url": "/MultiCall/static/css/main.04f3cf94.chunk.css"
+  },
+  {
+    "revision": "29438a79ee1009ec5376",
+    "url": "/MultiCall/static/js/2.ffcc15ac.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/MultiCall/static/js/2.ffcc15ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87bf140b417d74f3f2e",
+    "url": "/MultiCall/static/js/main.96b8a1a1.chunk.js"
+  },
+  {
+    "revision": "dbbbcc7acc062608d625",
+    "url": "/MultiCall/static/js/runtime-main.a8682417.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MultiCall/static/media/logo.5d5d9eef.svg"
+  }
+]);
