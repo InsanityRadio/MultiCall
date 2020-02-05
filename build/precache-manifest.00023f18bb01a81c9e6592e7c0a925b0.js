f2aa77b43c0f9269ee8eb6748b06e16daefaccb5
Jamie0 <jamie+git@index.hm>
2020-02-05 17:24:30 +0000

    build

---
+++ build/precache-manifest.00023f18bb01a81c9e6592e7c0a925b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac744d7c0903b0a3b7e88ae2923045ad",
+    "url": "/MultiCall/index.html"
+  },
+  {
+    "revision": "ddbe0e9c52c06cadb55d",
+    "url": "/MultiCall/static/css/main.04f3cf94.chunk.css"
+  },
+  {
+    "revision": "29438a79ee1009ec5376",
+    "url": "/MultiCall/static/js/2.ffcc15ac.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/MultiCall/static/js/2.ffcc15ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddbe0e9c52c06cadb55d",
+    "url": "/MultiCall/static/js/main.2d6a9f0c.chunk.js"
+  },
+  {
+    "revision": "dbbbcc7acc062608d625",
+    "url": "/MultiCall/static/js/runtime-main.a8682417.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MultiCall/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.75a56732088549171bc0b025a6d2e9a9.js"
+  "/MultiCall/precache-manifest.00023f18bb01a81c9e6592e7c0a925b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MultiCall/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
