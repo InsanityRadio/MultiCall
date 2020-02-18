9d4ea9e5c7ca19b9922b1231203353a2ee1d664f
Jamie0 <jamie+git@index.hm>
2020-02-18 10:05:39 +0000

    build with obfuscation

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MultiCall/precache-manifest.ff70b1cb156517b0724cca43ad3fa493.js"
+  "/MultiCall/precache-manifest.c9291795a5f8479ae51149809fca717c.js"
 );
 
 self.addEventListener('message', (event) => {
